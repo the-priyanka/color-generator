b6a2c7f347c648431f1b39761a50c06b87eb0ff2
Priyanka <original.manaria@gmail.com>
2022-02-07 15:47:22 +0530

    comlete this project

--- src/components/SingleColor.js
+++ src/components/SingleColor.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import rgbToHex from "./Untils";
 
 const SingleColor = (props) => {
@@ -6,13 +6,27 @@ const SingleColor = (props) => {
   const bcg = props.rgb.join(",");
   const hex = rgbToHex(...props.rgb);
   const hexValue = `#${props.hexColor}`;
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+
+    return () => clearTimeout(timeOut);
+  }, [alert]);
+
   return (
     <article
       className={`color ${props.index > 10 && "color-light"}`}
       style={{ background: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
     >
       <p className="percent-value">{props.weight}%</p>
       <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   );
 };
